337ad86d91b6c1db4dd6a266e2ef999033c6616b
Sumitjh26997 <sumit.hotchandnai@gmail.com>
2018-03-08 20:23:13 +0530

    10001st prime number Sieve of Eratosthenes

---
+++ p7.cpp
@@ -0,0 +1,48 @@
+/*
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+What is the 10001st prime number?
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    long count=0;
+    long limit=(10001*11)+1;
+    cout<<limit<<endl;
+
+    bool prime[limit+1];
+    for(long i=1;i<=limit;i++)
+    {
+        prime[i]=true;
+    }
+
+    for(long p=2;p<=limit;p++)
+    {
+        if(prime[p]==true)
+        {
+            for(long i=p*2;i<=limit;i=i+p)
+            {
+                prime[i]=false;
+            }
+        }
+    }
+
+    for(long i=2;i<=limit;i++)
+    {
+        if(prime[i])
+        {
+            count++;
+            cout<<count<<"="<<i<<endl;
+            if(count==10001)
+            {
+                break;
+            }
+        }
+        //cout<<i<<"="<<prime[i]<<endl;
+    }
+
+    return 0;
+}
